16d4fd074413bfe9accc3f6d6a0fd4fbdf24847e
Rama Vijaya Jhowry <ramajhowry@gmail.com>
2025-11-02 00:23:39 +0700

    Add files via upload

---
+++ simple_code.py
@@ -0,0 +1,14 @@
+team_profile = [
+    ["Ghizal Niko", "@Ghizal N.", "Germany", "Reading", "Munich University", "lacZ gene", "ATGACCATGATTACGGATTC"],
+    ["Vilvian Anunda", "@Vil", "Kenya", "Cooking", "University of Nairobi", "tfdA gene", "ATGACGCTGACCTTCCATGA"],
+    ["Rama Vijaya Jhowry", "@Rama Jhowry", "Indonesia", "Tasting food", "IPB University", "ggox gene", "ATGAAGAACTATCCTCTCCAG"],
+    ["Onyinye Vivian", "@xoxo, Vivianne", "Nigeria", "Watching movies", "FUTO", "HBB gene", "ATGGTGCACCTGACTCCTGA"],
+    ["Sreenidhi Vasudevan", "@Sreenidhi Vasudevan", "India", "Drawing", "Anna University", "SMN1 gene", "GCACCCGCGGGTTTGCTATG"],
+    ["Akash Madhusudhan", "@V.S.Akash", "India", "Carnatic Music", "Anna University", "HMGCR gene", "ATGGCCCGCGGGCGCGCGG"],
+    ["Temitope Olajide", "@Temitope Olajide", "Liberia", "Reading", "William V.S. Tubman University", "F8 gene", "ATCGCTGTGATGTTTTGACC"]
+]
+
+for team_member in team_profile:
+    for profile in team_member:
+        print(profile)
+    print("\n")
